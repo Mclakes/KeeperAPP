839584a2c7b7ad38d25fe6b2d629d1010bcde86e
Olalekan Bankole <60821157+Mclakes@users.noreply.github.com>
2022-04-25 21:46:30 +0100

    Initial commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const date = new Date();
+const currentTime = date.getFullYear();
+
+function Footer() {
+  return (
+    <footer>
+      <p>copyright © {currentTime}</p>;
+    </footer>
+  );
+}
+
+export default Footer;
